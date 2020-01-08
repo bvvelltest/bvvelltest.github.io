14fd0aad4f9af13c230cedd4e41c88cbbd11582a
Vladimir Bernacky <vladimir.bernacky@neotech.by>
2020-01-08 14:17:36 +0300

    update message sender id

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
 
 firebase.initializeApp({
-  messagingSenderId: '581169355573'
+  messagingSenderId: '178216728513'
 });
 const messaging = firebase.messaging();
 
